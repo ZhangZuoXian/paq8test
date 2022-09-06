015acf148c6d5e15ee9d66d00e58c665b5a5e134
ZZX' NDP <824595970@qq.com>
2022-09-06 16:06:04 +0800

    add block size = 0

--- src/paq8test.cpp
+++ src/paq8test.cpp
@@ -34,17 +34,17 @@ int main(int argc,char **argv){
 
     sscanf(argv[1], "%ld", &BLOCK_SIZE); //第一个参数为分块大小，单位为KB
     BLOCK_SIZE *= 1024; //单位换算为Byte
+    char filePath[128];
     if(strcmp(argv[2], "enwik8") == 0){ //根据第二个参数选择文件
-        fp = fopen("/home/zzx/paq8test/data/enwik8/enwik8","r");
+        sprintf(filePath, "/home/zzx/paq8test/data/enwik8/enwik8");
     }
     else if(strcmp(argv[2], "enwik9") == 0){
-        fp = fopen("/home/zzx/paq8test/data/enwik9","r");
+        sprintf(filePath, "/home/zzx/paq8test/data/enwik9");
     }
     else{
-        char filePath[64];
         sprintf(filePath, "/home/zzx/paq8test/data/calgarycorpus//%s", argv[2]);
-        fp = fopen(filePath, "r");
     }
+    fp = fopen(filePath, "r");
     if(fp == NULL){
         printf("File \"%s\" no exist\n", argv[2]);
         return 0;
@@ -59,13 +59,14 @@ int main(int argc,char **argv){
 
         FILE* out = fopen("/home/zzx/paq8test/originPaq8test/output.txt","w");
 
-        printf("Begin compression:....\n");
+        printf("Begin compression %s\n", argv[2]);
+        std::cout << "block size is : " << BLOCK_SIZE/1024 << " KB\n";
         Encoder *en = NULL;
 
         //统计文件大小
         fseek(fp, 0, SEEK_END);
         long size = ftell(fp);
-        std::cout << "the size is : " << size << "B\n";
+        std::cout << "original size is : " << size << " B\n";
         fseek(fp, 0, SEEK_SET);
 
         //压缩处理
@@ -79,7 +80,7 @@ int main(int argc,char **argv){
         while(notEnd){
             // printf("%x\t",c);
             en = new Encoder(&shared, COMPRESS, out, deCom);
-            for(byteCount = 0; byteCount < BLOCK_SIZE; byteCount++){
+            for(byteCount = 0; BLOCK_SIZE == 0 || byteCount < BLOCK_SIZE; byteCount++){
                 if((c=getc(fp)) == EOF){
                     notEnd = 0;
                     break;
@@ -92,14 +93,14 @@ int main(int argc,char **argv){
 
             sizeTmp = size;
             size = ftell(out);
-            std::cout<<"Block compressed size: "<<size - sizeTmp<<"B"<<std::endl;
+            // std::cout<<"Block compressed size: "<<size - sizeTmp<<"B"<<std::endl;
         }
         
         t = clock()-t;
         
         //输出压缩统计信息
         size = ftell(out);
-        std::cout<<"compressed size: "<<size<<"B"<<std::endl;
+        std::cout<<"compressed size: "<<size<<" B"<<std::endl;
         printf("compression time=%lf s\n",((float) t)/ CLOCKS_PER_SEC);
         fclose(out);
     } else {
@@ -117,5 +118,4 @@ int main(int argc,char **argv){
 
     fclose(fp);
     fclose(deCom);
-
 }
